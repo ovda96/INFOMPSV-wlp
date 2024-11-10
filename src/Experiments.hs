49505c94c00a80f3b6303be9b80880b0bc59d824
Martijn Voordouw <martijnvoordouw@gmail.com>
2024-11-11 00:26:09 +0100

    new experiment and new csv's

--- src/Experiments.hs
+++ src/Experiments.hs
@@ -1,16 +1,19 @@
 -- | Experiments.hs: Contains experiments for gathering data
+module Experiments
+  ( runExpirements,
+  )
+where
 
-module Experiments (
-  runExpirements
-  ) where
 --
-import Data.Bifunctor ( second )
-import Data.List ( intercalate )
+import Data.Bifunctor (second)
+import Data.List (intercalate)
+import GCLParser.GCLDatatype
+import GCLUtils (parseGCLfile)
 import Parse
-import System.Timeout ( timeout )
-import Text.Printf ( printf )
-import System.Random ( randomRIO )
 import PathTree
+import System.Random (randomRIO)
+import System.Timeout (timeout)
+import Text.Printf (printf)
 
 experimentFiles :: [String]
 experimentFiles =
@@ -62,14 +65,58 @@ functions =
 
 -- time out before we kill a run
 timeoutMS :: Int
-timeoutMS = 60 * 10 ^ (6 :: Int)
+timeoutMS = 2 * 60 * 10 ^ (6 :: Int)
 
 runExpirements :: IO ()
 runExpirements = do
   putStrLn "hello experiments"
-  experimentRunTimes "feasibility_strategys.csv" functions benchmarkFiles [30, 40, 45]
-  experimentRunTimes "length_time.csv" [("", randomChooseCheck)] benchmarkFiles [30, 60, 90]
-  experimentPaths "paths_time.csv" benchmarkFiles [30, 60, 90]
+  -- experimentRunTimes "feasibility_strategys.csv" functions benchmarkFiles [30, 40, 45]
+  -- experimentRunTimes "length_time.csv" [("", randomChooseCheck)] benchmarkFiles [30, 60, 90]
+  -- experimentPaths "paths_time.csv" benchmarkFiles [30, 60, 90]
+  -- experimentBenchmarks "examples/benchmark/memberOf.gcl" [40, 55, 65] "bench_member.csv"
+  experimentBenchmarks "examples/benchmark/divByN.gcl" [100, 150, 200] "bench_div.csv"
+  -- experimentBenchmarks "examples/benchmark/pullUp.gcl" [100] "bench_pull.csv"
+
+experimentBenchmarks :: String -> [Int] -> String -> IO ()
+experimentBenchmarks file pathLengths filename =
+  do
+    program <- parseGCLfile file
+    let prg' = fromRight program
+    print prg'
+    results <-
+      sequence
+        [ do
+            print nam
+            lst <- sequence [timeout timeoutMS (runWithProgram randomChooseCheck False len False prg) | len <- pathLengths]
+            return (nam, lst)
+          | (nam, prg) <- generateExperiments prg'
+        ]
+    let runtimes = map (second $ map (showTime . fmap runtime)) results
+    toCsvFile filename (("lengths", map show pathLengths) : runtimes)
+  where
+    generateExperiments :: Program -> [(String, Program)]
+    generateExperiments
+      Program
+        { name = n,
+          input = i,
+          output = out,
+          stmt = Seq (Assign "N" _) xs
+        } =
+        [ ( show ns,
+            Program
+              { name = n,
+                input = i,
+                output = out,
+                stmt = Seq (Assign "N" (LitI ns)) xs
+              }
+          )
+          | ns <- [2 .. 10]
+        ]
+    generateExperiments _ = error "idk"
+
+    fromRight :: Either a b -> b
+    fromRight (Right x) = x
+    fromRight (Left _) = error "parsing error"
 
 experimentRunTimes :: String -> [(String, Int -> Int -> IO Bool)] -> [String] -> [Int] -> IO ()
 experimentRunTimes filename fs files pathLengths =
